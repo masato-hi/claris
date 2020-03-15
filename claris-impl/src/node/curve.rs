964d898949d70187e4bfe3dbfe99b971e441d8dd
masato <masato.hirahata@gmail.com>
2020-03-16 02:21:20 +0900

    add curve tests

--- claris-impl/src/node/curve.rs
+++ claris-impl/src/node/curve.rs
@@ -64,3 +64,259 @@ impl Curve {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Curve;
+    use crate::parse_yaml;
+    use cairo::LineCap;
+    use float_cmp::approx_eq;
+    use yaml_rust::YamlLoader;
+
+    macro_rules! parse {
+        ($x:expr) => {{
+            let src = parse_yaml!($x);
+            match Curve::parse(&src) {
+                Ok(x) => x,
+                Err(e) => panic!(e.to_string()),
+            }
+        }};
+    }
+
+    #[test]
+    #[should_panic(expected = "'curve' is required 'color' option")]
+    fn color_is_blank() {
+        let s = "---
+start: [10, 5]
+midway: [30, 40]
+end: [10, 60]
+";
+        parse!(s);
+    }
+
+    #[test]
+    fn color_is_rgb() {
+        let s = "---
+color: '#AABBCC'
+start: [10, 5]
+midway: [30, 40]
+end: [10, 60]
+";
+        let subject = parse!(s);
+        assert_eq!(subject.color.r, 170);
+        assert_eq!(subject.color.g, 187);
+        assert_eq!(subject.color.b, 204);
+        assert!(approx_eq!(f32, subject.color.a, 1.0));
+    }
+
+    #[test]
+    fn color_and_alpha() {
+        let s = "---
+color: '#AABBCC'
+alpha: 0.5
+start: [10, 5]
+midway: [30, 40]
+end: [10, 60]
+";
+        let subject = parse!(s);
+        assert_eq!(subject.color.r, 170);
+        assert_eq!(subject.color.g, 187);
+        assert_eq!(subject.color.b, 204);
+        assert!(approx_eq!(f32, subject.color.a, 0.5));
+    }
+
+    #[test]
+    fn stroke_is_blank() {
+        let s = "---
+color: '#AABBCC'
+start: [10, 5]
+midway: [30, 40]
+end: [10, 60]
+";
+        let subject = parse!(s);
+        assert!(approx_eq!(f64, subject.stroke.width, 1.0));
+        assert_eq!(subject.stroke.cap, LineCap::Butt);
+    }
+
+    #[test]
+    fn stroke_with_integer_width() {
+        let s = "---
+color: '#AABBCC'
+stroke:
+  width: 2
+start: [10, 5]
+midway: [30, 40]
+end: [10, 60]
+";
+        let subject = parse!(s);
+        assert!(approx_eq!(f64, subject.stroke.width, 2.0));
+        assert_eq!(subject.stroke.cap, LineCap::Butt);
+    }
+
+    #[test]
+    fn stroke_with_float_width() {
+        let s = "---
+color: '#AABBCC'
+stroke:
+  width: 2.5
+start: [10, 5]
+midway: [30, 40]
+end: [10, 60]
+";
+        let subject = parse!(s);
+        assert!(approx_eq!(f64, subject.stroke.width, 2.5));
+        assert_eq!(subject.stroke.cap, LineCap::Butt);
+    }
+
+    #[test]
+    fn stroke_with_cap() {
+        let s = "---
+color: '#AABBCC'
+stroke:
+  cap: round
+start: [10, 5]
+midway: [30, 40]
+end: [10, 60]
+";
+        let subject = parse!(s);
+        assert!(approx_eq!(f64, subject.stroke.width, 1.0));
+        assert_eq!(subject.stroke.cap, LineCap::Round);
+    }
+
+    #[test]
+    fn scale_is_blank() {
+        let s = "---
+color: '#AABBCC'
+start: [10, 5]
+midway: [30, 40]
+end: [10, 60]
+";
+        let subject = parse!(s);
+        assert!(approx_eq!(f64, subject.scale.x, 1.0));
+        assert!(approx_eq!(f64, subject.scale.y, 1.0));
+    }
+
+    #[test]
+    fn scale_is_not_blank() {
+        let s = "---
+color: '#AABBCC'
+scale:
+  x: 2
+  y: 2.5
+start: [10, 5]
+midway: [30, 40]
+end: [10, 60]
+";
+        let subject = parse!(s);
+        assert!(approx_eq!(f64, subject.scale.x, 2.0));
+        assert!(approx_eq!(f64, subject.scale.y, 2.5));
+    }
+
+    #[test]
+    #[should_panic(expected = "'curve' is required 'start' option")]
+    fn start_is_blank() {
+        let s = "---
+color: '#AABBCC'
+midway: [30, 40]
+end: [10, 60]
+";
+        parse!(s);
+    }
+
+    #[test]
+    fn start_is_not_blank() {
+        let s = "---
+color: '#AABBCC'
+start: [10, 5]
+midway: [30, 40]
+end: [10, 60]
+";
+        let subject = parse!(s);
+        assert!(approx_eq!(f64, subject.start.x, 10.0));
+        assert!(approx_eq!(f64, subject.start.y, 5.0));
+    }
+
+    #[test]
+    #[should_panic(expected = "invalid point")]
+    fn start_is_invalid() {
+        let s = "---
+color: '#AABBCC'
+start: [10]
+midway: [30, 40]
+end: [10, 60]
+";
+        parse!(s);
+    }
+
+    #[test]
+    #[should_panic(expected = "'curve' is required 'midway' option")]
+    fn midway_is_blank() {
+        let s = "---
+color: '#AABBCC'
+start: [10, 5]
+end: [10, 60]
+";
+        parse!(s);
+    }
+
+    #[test]
+    fn midway_is_not_blank() {
+        let s = "---
+color: '#AABBCC'
+start: [10, 5]
+midway: [30, 40]
+end: [10, 60]
+";
+        let subject = parse!(s);
+        assert!(approx_eq!(f64, subject.midway.x, 30.0));
+        assert!(approx_eq!(f64, subject.midway.y, 40.0));
+    }
+
+    #[test]
+    #[should_panic(expected = "invalid point")]
+    fn midway_is_invalid() {
+        let s = "---
+color: '#AABBCC'
+start: [10, 5]
+midway: [30]
+end: [10, 60]
+";
+        parse!(s);
+    }
+
+    #[test]
+    #[should_panic(expected = "'curve' is required 'end' option")]
+    fn end_is_blank() {
+        let s = "---
+color: '#AABBCC'
+start: [10, 5]
+midway: [30, 40]
+";
+        parse!(s);
+    }
+
+    #[test]
+    fn end_is_not_blank() {
+        let s = "---
+color: '#AABBCC'
+start: [10, 5]
+midway: [30, 40]
+end: [10, 60]
+";
+        let subject = parse!(s);
+        assert!(approx_eq!(f64, subject.end.x, 10.0));
+        assert!(approx_eq!(f64, subject.end.y, 60.0));
+    }
+
+    #[test]
+    #[should_panic(expected = "invalid point")]
+    fn end_is_invalid() {
+        let s = "---
+color: '#AABBCC'
+start: [10, 5]
+midway: [30, 40]
+end: [10]
+";
+        parse!(s);
+    }
+}
