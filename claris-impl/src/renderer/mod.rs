1a802c0810911675c797e20e5ec6fcae31daebd8
masato <masato.hirahata@gmail.com>
2020-03-23 00:55:15 +0900

    add renderer tests

--- claris-impl/src/renderer/mod.rs
+++ claris-impl/src/renderer/mod.rs
@@ -52,3 +52,82 @@ pub fn render(context: &mut dyn Context, layer: Layer) {
     };
     context.restore();
 }
+
+#[cfg(test)]
+mod tests {
+    use super::render;
+    use crate::node::Arc;
+    use crate::node::Circle;
+    use crate::node::Curve;
+    use crate::node::Layer;
+    use crate::node::Line;
+    use crate::node::Polygon;
+    use crate::node::Rectangle;
+    use crate::node::Text;
+    use crate::node::Triangle;
+    use crate::testing_helpers::stub::ContextImpl;
+
+    #[test]
+    fn arc() {
+        let mut context = ContextImpl::new();
+        let node = Arc::default();
+        let layer = Layer::Arc(node);
+        render(&mut context, layer);
+    }
+
+    #[test]
+    fn circle() {
+        let mut context = ContextImpl::new();
+        let node = Circle::default();
+        let layer = Layer::Circle(node);
+        render(&mut context, layer);
+    }
+
+    #[test]
+    fn curve() {
+        let mut context = ContextImpl::new();
+        let node = Curve::default();
+        let layer = Layer::Curve(node);
+        render(&mut context, layer);
+    }
+
+    #[test]
+    fn line() {
+        let mut context = ContextImpl::new();
+        let node = Line::default();
+        let layer = Layer::Line(node);
+        render(&mut context, layer);
+    }
+
+    #[test]
+    fn polygon() {
+        let mut context = ContextImpl::new();
+        let node = Polygon::default();
+        let layer = Layer::Polygon(node);
+        render(&mut context, layer);
+    }
+
+    #[test]
+    fn rectangle() {
+        let mut context = ContextImpl::new();
+        let node = Rectangle::default();
+        let layer = Layer::Rectangle(node);
+        render(&mut context, layer);
+    }
+
+    #[test]
+    fn text() {
+        let mut context = ContextImpl::new();
+        let node = Text::default();
+        let layer = Layer::Text(node);
+        render(&mut context, layer);
+    }
+
+    #[test]
+    fn triangle() {
+        let mut context = ContextImpl::new();
+        let node = Triangle::default();
+        let layer = Layer::Triangle(node);
+        render(&mut context, layer);
+    }
+}
